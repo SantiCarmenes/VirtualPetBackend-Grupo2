94416df756c71d6de5b9e390182a24fc6f8f4b34
Gian Franco <gianmagliotti03@gmail.com>
2026-05-14 22:59:49 -0300

    prisma config

--- backend-virtualpet/src/main.ts
+++ backend-virtualpet/src/main.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
+import * as path from 'path';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
